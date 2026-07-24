7dda89de8d30f3c9ba1144b31c8c8c4044b4f24e
Yongli He <heyongli@gmail.com>
2026-07-24 12:16:48 -0700

    Rename uart_dev.c to serial.c with consistent serial_dev API
    - rename uart_dev.c to serial.c
    - rename uart_dev.h to serial_dev.h
    - update serial.c to use serial_dev_* function names
    - update kernel/console.c to include serial_dev.h
    - update Makefile build rule dependency

--- Makefile
+++ Makefile
@@ -27,7 +27,7 @@ $(BDIR)/rtc.o: bmX86/rtc.c bmX86/rtc.h | $(BDIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BDIR)/uart.o: bmX86/uart.c bmX86/include/uart.h | $(BDIR)
-$(BDIR)/serial.o: bmX86/serial.c bmX86/include/uart_dev.h | $(BDIR)
+$(BDIR)/serial.o: bmX86/serial.c bmX86/include/serial_dev.h | $(BDIR)
 
 	$(CC) $(CFLAGS) -c $< -o $@
 	$(CC) $(CFLAGS) -c $< -o $@
--- bmX86/serial.c
+++ bmX86/serial.c
@@ -1,29 +1,29 @@
 #include "include/uart_dev.h"
 #include "include/uart.h"
 
-static struct uart_dev dev;
+static struct serial_dev dev;
 static int dev_initialized = 0;
 
-static void uart_dev_write(const char *s, int len) {
+static void serial_dev_write(const char *s, int len) {
     uart_write_buf(s, len);
 }
 
-static void uart_dev_flush(void) {
+static void serial_dev_flush(void) {
     /* No hardware flush needed for 16550 UART */
 }
 
-void uart_dev_init(void) {
+void serial_dev_init(void) {
     if (!dev_initialized) {
-        dev.write = uart_dev_write;
-        dev.flush = uart_dev_flush;
+        dev.write = serial_dev_write;
+        dev.flush = serial_dev_flush;
         dev_initialized = 1;
     }
 }
 
-void uart_dev_write(const char *s, int len) {
+void serial_dev_write(const char *s, int len) {
     uart_write_buf(s, len);
 }
 
-void uart_dev_flush(void) {
+void serial_dev_flush(void) {
     /* No hardware flush needed for 16550 UART */
 }
